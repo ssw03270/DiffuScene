3293ebf8c1f0acd9aff5084dcc435e51dbfb655a
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2025-03-12 09:42:03 +0900

    Update train_text.sh

--- run/train_text.sh
+++ run/train_text.sh
@@ -1,18 +1,37 @@
+#!/usr/bin/bash
+#SBATCH -J diffusion_train
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem-per-gpu=29G
+#SBATCH -p batch_grad
+#SBATCH -w ariel-v2
+#SBATCH -t 1-0
+#SBATCH -o logs/slurm-%A.out
+
+# 현재 작업 디렉토리 확인
+pwd
+which python
+hostname
+
+# scripts 폴더로 이동
 cd ./scripts
 
+# 결과 디렉토리 지정
 exp_dir="outputs"
 
 #### bedrooms
 config="../config/text/diffusion_bedrooms_instancond_lat32_v_bert.yaml"
 exp_name="diffusion_bedrooms_instancond_lat32_v_bert"
-python train_diffusion.py $config $exp_dir --experiment_tag $exp_name  --with_wandb_logger
+python train_diffusion.py $config $exp_dir --experiment_tag $exp_name --with_wandb_logger
 
 #### diningrooms
 config="../config/uncond/diffusion_diningrooms_instancond_lat32_v_bert.yaml"
 exp_name="diffusion_diningrooms_instancond_lat32_v_bert"
-python train_diffusion.py $config $exp_dir --experiment_tag $exp_name  --with_wandb_logger
+python train_diffusion.py $config $exp_dir --experiment_tag $exp_name --with_wandb_logger
 
 #### livingrooms
 config="../config/uncond/diffusion_livingrooms_instancond_lat32_v_bert.yaml"
 exp_name="diffusion_livingrooms_instancond_lat32_v_bert"
-python train_diffusion.py $config $exp_dir --experiment_tag $exp_name  --with_wandb_logger
+python train_diffusion.py $config $exp_dir --experiment_tag $exp_name --with_wandb_logger
+
+exit 0
