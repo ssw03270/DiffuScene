6ac8bb4b665baed689c1bf428f7cee1d0d130af0
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2025-03-14 16:36:57 +0900

    Update generate_text.sh

--- run/generate_text.sh
+++ run/generate_text.sh
@@ -1,33 +1,29 @@
+#!/usr/bin/bash
+#SBATCH -J diffusion_train
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem-per-gpu=29G
+#SBATCH -p batch_grad
+#SBATCH -w ariel-v2
+#SBATCH -t 1-0
+#SBATCH -o logs/slurm-%A.out
+
+# 현재 작업 디렉토리 확인
+pwd
+which python
+hostname
+
+# scripts 폴더로 이동
 cd ./scripts
 
-
-exp_dir="../pretrained"
+exp_dir="outputs"
 
 ####'bedrooms'
 config="../config/text/diffusion_bedrooms_instancond_lat32_v_bert.yaml"
-exp_name="bedrooms_bert"
-weight_file=$exp_dir/$exp_name/$exp_name.pt
-threed_future='/cluster/balrog/jtang/3d_front_processed/bedrooms/threed_future_model_bedroom.pkl'
-
-python  generate_diffusion.py $config  $exp_dir/$exp_name/gen_top2down_notexture_nofloor $threed_future  --weight_file $weight_file \
-    --without_screen  --n_sequences 1000 --render_top2down --save_mesh --no_texture --without_floor  --clip_denoised --retrive_objfeats
-
-
-####'diningrooms'
-config="../config/text/diffusion_diningrooms_instancond_lat32_v_bert.yaml"
-exp_name="diningrooms_bert"
-weight_file=$exp_dir/$exp_name/$exp_name.pt
-threed_future='/cluster/balrog/jtang/3d_front_processed/diningrooms/threed_future_model_diningroom.pkl'
-
-python  generate_diffusion.py $config  $exp_dir/$exp_name/gen_top2down_notexture_nofloor $threed_future  --weight_file $weight_file \
-    --without_screen  --n_sequences 1000 --render_top2down --save_mesh --no_texture --without_floor  --clip_denoised --retrive_objfeats
-
-
-####'livingrooms'
-config="../config/text/diffusion_livingrooms_instancond_lat32_v.yaml"
-exp_name="livingrooms_bert"
-weight_file=$exp_dir/$exp_name/$exp_name.pt
-threed_future='/cluster/balrog/jtang/3d_front_processed/livingrooms/threed_future_model_livingroom.pkl'
+exp_name="diffusion_bedrooms_instancond_lat32_v_bert"
+model_name="model_5000"
+weight_file=$exp_dir/$exp_name/$model_name.pt
+threed_future='../dataset/3d_front_processed/threed_future_model_bedroom.pkl'
 
 python  generate_diffusion.py $config  $exp_dir/$exp_name/gen_top2down_notexture_nofloor $threed_future  --weight_file $weight_file \
     --without_screen  --n_sequences 1000 --render_top2down --save_mesh --no_texture --without_floor  --clip_denoised --retrive_objfeats
